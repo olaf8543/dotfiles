85a3b66103a148d97ddd2431c1ca0bc51ca8b2f8
olaf8543 <179398174+olaf8543@users.noreply.github.com>
2025-04-21 23:30:53 -0400

    finished adding git in nvim

--- config/nvim/lua/plugins/floatterm.lua
+++ config/nvim/lua/plugins/floatterm.lua
@@ -1,6 +1,11 @@
 return {
     "voldikss/vim-floaterm",
-
+    keys = {
+        {
+            "<leader>g",
+            "<cmd>FloatermNew --width=0.9 lazygit<CR>",
+        },
+    },
     config = function()
     end
 }
