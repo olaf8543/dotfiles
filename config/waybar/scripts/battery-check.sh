b9b067a19edc1702ef901e91931138b799d797cb
olaf8543 <179398174+olaf8543@users.noreply.github.com>
2025-04-30 18:33:17 -0400

    added dynamic notification menu

--- config/waybar/scripts/battery-check.sh
+++ config/waybar/scripts/battery-check.sh
@@ -5,11 +5,11 @@ BAT_STATUS=$(cat /sys/class/power_supply/BAT0/status)
 STATE_FILE="/tmp/low_battery_notified"
 
 # If battery is low and discharging
-if [ "$BAT_STATUS" = "Discharging" ] && [ "$BAT_CAPACITY" -lt 20 ]; then
+if [ "$BAT_STATUS" = "Discharging" ] && [ "$BAT_CAPACITY" -le 20 ]; then
     # Only send notification if it hasn’t been sent already
     if [ ! -f "$STATE_FILE" ]; then
         export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
-        export DISPLAY=:0  # Required for Hyprland/Wayland (may need WAYLAND_DISPLAY)
+        export DISPLAY=:0
         dunstify -u critical "BATTERY LOW: ${BAT_CAPACITY}%"
         touch "$STATE_FILE"  # Create a state file to block future notifications
     fi
