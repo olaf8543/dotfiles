90cbe2f64f32dc02d2d135e3c57c81cf998b37cb
olaf8543 <179398174+olaf8543@users.noreply.github.com>
2025-05-10 12:09:03 -0400

    finally should be done configuring my vscode nvim which luckily didnt take very long

--- config/nvim/lua/vsoptions.lua
+++ config/nvim/lua/vsoptions.lua
@@ -3,21 +3,20 @@ vim.opt.clipboard = "unnamedplus"
 
 vim.keymap.set('n', '<leader>al', function()
     vim.fn.VSCodeNotify('settings.cycle.lineNumbers')
-end)
+end, { desc = 'Change between relative and absolute line numbers' })
 
 vim.keymap.set('n', '<leader>e', function()
     vim.fn.VSCodeNotify('workbench.view.explorer') -- Focus the explorer
 end, { desc = 'Focus Explorer sidebar' })
 
-vim.keymap.set('n', '<leader>E', function()
+vim.keymap.set('n', '<leader>d', function()
     vim.fn.VSCodeNotify('workbench.action.toggleSidebarVisibility') -- Focus the explorer
-end, { desc = 'Focus Explorer sidebar' })
+end, { desc = 'Toggle Explorer sidebar visibility' })
 
 vim.keymap.set('n', '<leader>g', function()
-    vim.fn.VSCodeNotify('workbench.action.terminal.new')
-    vim.fn.VSCodeNotify('workbench.action.terminal.sendSequence', {
-        text = 'lazygit\n'
-    })
-    -- Focus the terminal pane
+    vim.fn.VSCodeNotify('multiCommand.lazygitFlow')
+end, { desc = 'Open lazygit and auto-close terminal' })
+
+vim.keymap.set('n', '<leader>t', function()
     vim.fn.VSCodeNotify('workbench.action.terminal.focus')
-end, { desc = 'Open lazygit in terminal' })
+end, { desc = 'Focus terminal' })
