00776ee52f11f3ffdf794cbeda661008c77a1864
olaf8543 <179398174+olaf8543@users.noreply.github.com>
2025-11-02 09:31:55 -0500

    added yazi

---
+++ config/yazi/init.lua
@@ -0,0 +1,19 @@
+require("relative-motions"):setup({ show_numbers = "relative", show_motion = true, enter_mode = "first" })
+
+require("bookmarks"):setup({
+	last_directory = { enable = false, persist = false, mode = "dir" },
+	persist = "all",
+	desc_format = "parent",
+	file_pick_mode = "hover",
+	custom_desc_input = false,
+	show_keys = false,
+	notify = {
+		enable = false,
+		timeout = 1,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+			delete_all = "Deleted all bookmarks",
+		},
+	},
+})
